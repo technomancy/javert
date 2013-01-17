8aa6df4030210261b4768366d46f27550942dcbf
Brian Rowe <briprowe@yahoo.com>
2013-01-17 14:40:26 -0500

    Add elisp to inspect items from an emacs buffer.

---
+++ nrepl-inspect.el
@@ -0,0 +1,32 @@
+(require 'cl)
+(require 'nrepl)
+
+(defun nrepl-inspect-print-result (buffer str)
+  (with-current-buffer buffer
+    (let ((inhibit-read-only t))
+      (insert str))))
+
+(defun debug (output)
+  (with-current-buffer (get-buffer-create "nrepl-inspect-debug")
+    (if (= (point) (point-max))
+        (insert output))
+    (save-excursion
+      (goto-char (point-max))
+      (insert output))))
+
+(defun nrepl-inspect-request (sym ns)
+  (list "op" "inspect" "sym" sym "ns" ns))
+
+(defun nrepl-inspect ()
+  (interactive)
+  (let ((sym (format "%s" (symbol-at-point)))
+        (inspect-buffer (nrepl-popup-buffer "*nREPL inspect*" t)))
+    (nrepl-send-request (nrepl-inspect-request sym nrepl-buffer-ns)
+                        (nrepl-make-response-handler
+                         inspect-buffer
+                         (lambda (buffer str)
+                            (nrepl-inspect-print-result buffer value))
+                         '()
+                         (lambda (buffer str)
+                           (nrepl-emit-into-popup-buffer buffer "Error"))
+                         '()))))
